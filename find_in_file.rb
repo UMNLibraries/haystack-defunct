1bea8bb852c3a843984e865c0fb246fe751a0eaa
chadfennell <libsys@gmail.com>
2017-01-12 12:47:39 -0600

    legacy code expects a pattern hash

--- find_in_file.rb
+++ find_in_file.rb
@@ -24,7 +24,7 @@ module Haystack
     end
 
     def terms
-      @term_data ||= Oj.load(Net::HTTP.get_response(terms_uri).body).join('|')
+      @term_data ||= Oj.load(Net::HTTP.get_response(terms_uri).body)['pattern'].join('|')
     end
 
     def parse(record_string)
